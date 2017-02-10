8524415d593196e70702f81f8a7f3675339e9fc4
krishna <manish.t13@iiits.in>
2017-02-10 10:43:22 +0530

    assigments are updated

---
+++ Assignments/Assignment 2/concat.groovy
@@ -0,0 +1,9 @@
+class concate {
+   static void main(String[] args) {
+      String a = it.readLine();
+      String b = it.readLine();
+		
+     // println("Hello" + "World");
+      println(a + b);
+   }
+}
